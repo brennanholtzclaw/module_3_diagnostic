785e300b65a605b0865a16770b15b9a6e31f5216
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-18 10:26:20 -0600

    Failed to send a successful request
    * I guess I have no idea how to use faraday
    * Could not parse JSON
    * Didn't complete the first user story
    * Going to alter and seriously change scope of final project

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,11 @@
 class SearchController < ApplicationController
-
+  # respond_to :json
   def index
-    connection = Faraday.new(:url => "http://www.nrel.gov/api/alt-fuel-stations/v1/nearest.json?&api_key=#{ENV["NREL_KEY"]}&zip?#{params[:zipcode]}")
-    binding.pry
+    connection = Faraday.new(:url => "http://www.nrel.gov/api/alt-fuel-stations/v1/nearest.json?&api_key=#{ENV["NREL_KEY"]}&zip?#{params[:zipcode]}&limit=10")
+binding.pry
     @results = JSON.parse(connection.get.body)
-# binding.pry
+    # respond_with @results
+
   end
 
 end
