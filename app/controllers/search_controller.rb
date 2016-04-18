06d730faecf4597344c41318f41805506070de27
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-18 10:31:00 -0600

    change '?' t '='

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   # respond_to :json
   def index
-    connection = Faraday.new(:url => "http://www.nrel.gov/api/alt-fuel-stations/v1/nearest.json?&api_key=#{ENV["NREL_KEY"]}&zip?#{params[:zipcode]}&limit=10")
+    connection = Faraday.new(:url => "http://www.nrel.gov/api/alt-fuel-stations/v1/nearest.json?&api_key=#{ENV["NREL_KEY"]}&zip=#{params[:zipcode]}&limit=10")
 binding.pry
     @results = JSON.parse(connection.get.body)
     # respond_with @results
